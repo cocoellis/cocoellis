02e93f8b697835f20c87c98e467d88261bb67e55
Coco Ellis <mellis1223@gmail.com>
2019-06-10 18:46:21 -0500

    fixed age bug

--- app/components/Stats/Personal.js
+++ app/components/Stats/Personal.js
@@ -8,7 +8,7 @@ const PersonalStats = () => {
 
   const tick = () => {
     const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
-    const birthTime = new Date('1990-02-05T09:24:00');
+    const birthTime = new Date('1993-12-23T09:24:00');
     setData(Object.assign({}, data, {
       age: {
         label: 'Current age',
