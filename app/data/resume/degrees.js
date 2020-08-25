d50f2c02646acfc87b1c313c22c74f18c87dc039
cocoellis <38797064+cocoellis@users.noreply.github.com>
2020-08-25 18:10:56 -0500

    Update degrees.js

--- app/data/resume/degrees.js
+++ app/data/resume/degrees.js
@@ -1,10 +1,15 @@
 const degrees = [
   {
-      school: 'Bloc',
+      school: 'Thinkful',
       degree: 'Web Development Bootcamp',
       link: 'https://bloc.io',
       year: 2019,
     },
+  {
+    school: 'Tech Talent South',
+    degree: 'JavaScript, iOS Development',
+    link: 'https://techtalentsouth.com',
+    year: 2018,
     {
       school: 'Washington and Lee University',
       degree: 'B.S. Economics and Business Administration',
