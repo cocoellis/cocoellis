b423834a1907b62458e6ebb59dba896c815ac019
cocoellis <38797064+cocoellis@users.noreply.github.com>
2020-08-25 18:11:57 -0500

    Update github.js

--- app/data/github.js
+++ app/data/github.js
@@ -14,27 +14,7 @@ export const githubKeys = [
 
 // TODO To be provided by an API
 const data = [
-  {
-    label: 'Stars this repository has on github',
-    key: 'stargazers_count',
-    value: '1',
-    link: 'https://github.com/cocoellis/cocoellis/stargazers',
-  }, {
-    label: 'Number of people watching this repository',
-    key: 'subscribers_count',
-    value: '1',
-    link: 'https://github.com/cocoellis/cocoellis/stargazers',
-  }, {
-    label: 'Number of forks',
-    key: 'forks',
-    value: '0',
-    link: 'https://github.com/cocoellis/cocoellis/network',
-  }, {
-    label: 'Open github issues',
-    key: 'open_issues_count',
-    value: '0',
-    link: 'https://github.com/cocoellis/cocoellis/issues',
-  }, {
+{
     label: 'Last updated',
     key: 'pushed_at',
     value: dayjs().format('MMMM D, YYYY'),
